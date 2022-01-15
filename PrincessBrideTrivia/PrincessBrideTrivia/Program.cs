0cf3af3e668dbb4cbb030c10f49eadf3f1634882
mpennock4 <84542559+mpennock4@users.noreply.github.com>
2022-01-14 19:24:04 -0800

    Used explicit casting for percentage calculation to preserve precision

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -24,7 +24,8 @@ namespace PrincessBrideTrivia
 
         public static string GetPercentCorrect(int numberCorrectAnswers, int numberOfQuestions)
         {
-            return (numberCorrectAnswers / numberOfQuestions * 100) + "%";
+            double decimalPercent = (double)numberCorrectAnswers / numberOfQuestions;
+            return (int)(decimalPercent * 100) + "%";
         }
 
         public static bool AskQuestion(Question question)
