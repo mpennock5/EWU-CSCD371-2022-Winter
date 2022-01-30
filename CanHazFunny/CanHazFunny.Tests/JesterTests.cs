fa7848070db3102cb96cce23c0ca93ffdc715170
Aedyn01 <60992516+Aedyn01@users.noreply.github.com>
2022-01-30 05:27:23 -0800

    Added two interfaces, and new class

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,9 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CanHazFunny.Tests
+namespace CanHazFunny.Tests;
+
+[TestClass]
+public class JesterTests
 {
-    [TestClass]
-    public class JesterTests
+    [TestMethod]
+    public void Jester_GetJokeReturnsAJoke_True()
+    {
+        // MockThing is not working yet
+        // Jester joke = new MockThing("This is a joke");
+        // Assert.AreEqual<string>("This is a joke", joke.GetJoke);
+    }
+
+    [TestMethod]
+    public void Jester_TellJokeReturnsAValidJoke_True()
     {
         
     }
