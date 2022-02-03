f1405a5bdeeec9e80a177781f733999e7d7d63ac
mpennock5 <mpennock6@outlook.com>
2022-02-02 18:40:26 -0800

    Tested the check for nullability.

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 
 namespace CanHazFunny.Tests;
 
@@ -11,9 +12,9 @@ public class JesterTests
     [TestMethod]
     public void Jester_HasJokeOutputDependancy_True()
     {
-        JokeService jokeService = new JokeService();
-        JokeOutput jokeOutput = new JokeOutput();
-        Jester jester = new Jester(jokeOutput, jokeService);
+        JokeService jokeService = new();
+        JokeOutput jokeOutput = new();
+        Jester jester = new(jokeOutput, jokeService);
 
         Assert.AreEqual<IJokeOutput>(jester.JokeOutput, jokeOutput);
     }
@@ -21,9 +22,9 @@ public class JesterTests
     [TestMethod]
     public void Jester_HasJokeServiceDependancy_True()
     {
-        JokeService jokeService = new JokeService();
-        JokeOutput jokeOutput = new JokeOutput();
-        Jester jester = new Jester(jokeOutput, jokeService);
+        JokeService jokeService = new();
+        JokeOutput jokeOutput = new();
+        Jester jester = new(jokeOutput, jokeService);
 
         Assert.AreEqual<IJokeService>(jester.JokeService, jokeService);
 
@@ -31,15 +32,41 @@ public class JesterTests
     [TestMethod]
     public void Jester_JokeDoesNotContainChuckNorris_True()
     {
-        JokeService jokeService = new JokeService();
-        JokeOutput jokeOutput = new JokeOutput();
-        Jester jester = new Jester(jokeOutput, jokeService);
+        JokeService jokeService = new();
+        JokeOutput jokeOutput = new();
+        Jester jester = new(jokeOutput, jokeService);
 
         jester.TellJoke();
 
         Assert.IsFalse(jester.Joke!.Contains("Chuck Norris"));
 
     }
-    
+    [TestMethod]
+    public void Jester_IfJokeServiceIsNullThrowException_Pass()
+    {
+        JokeService? jokeService = null;
+        JokeOutput? jokeOutput = new();
+
+
+
+        Jester jester;
+        Assert.ThrowsException<ArgumentNullException>(() => jester = new(jokeOutput, jokeService!));
+
+
+    }
+    [TestMethod]
+    public void Jester_IfJokeOutputIsNullThrowException_Pass()
+    {
+        JokeService? jokeService = new();
+        JokeOutput? jokeOutput = null;
+
+
+
+        Jester jester;
+        Assert.ThrowsException<ArgumentNullException>(() => jester = new(jokeOutput!, jokeService!));
+
+
+    }
+
 }   
      
