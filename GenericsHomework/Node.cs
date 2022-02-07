f9bdaca865d591b817bc6129fd3bed74f079bb25
Aedyn01 <60992516+Aedyn01@users.noreply.github.com>
2022-02-07 13:33:03 -0800

    added methods and unit test class

---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,30 @@
+﻿namespace GenericsHomework;
+public class Node
+{
+    public Node(object node)
+    {
+        _Node = node;
+    }
+
+    public object _Node { get; set; }
+
+    public void Append(object value)
+    {
+
+    }
+
+    public void Clear(Node current)
+    {
+
+    }
+
+    public Boolean Exists(object value)
+    {
+        return false;
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ GenericsHomeworkTests/NodeTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsHomeworkTests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
