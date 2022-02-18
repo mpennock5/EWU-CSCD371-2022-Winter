26b7c16688b0a63ab7868bd3ba3cecc2583332d3
mpennock5 <mpennock6@outlook.com>
2022-02-18 13:55:39 -0800

    implemented csvRows(). Double check caching

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests;
+
+[Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+public class SampleDataTests
+{
+    [TestMethod]
+    public void CSVRows()
+    {
+        SampleData sample = new SampleData();
+        List<string> rows = (List<string>)sample.CsvRows;
+
+        Assert.AreEqual<string>("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577", rows[0]);
+       
+    }
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        private IEnumerable<string>? _CsvRows;
+        private string peopleCSV = System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("Assignment.dll", "People.csv");
+
+        public SampleData()
+        {
+            IEnumerable<string> CSV = File.ReadAllLines(peopleCSV);
+            CsvRows = CSV;
+        }
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        // Returns each row as a single string. Read up on chapter 13 and 15 for syntax
+        public IEnumerable<string> CsvRows 
+        { 
+            get
+            {
+                return _CsvRows ?? throw new ArgumentNullException();
+            }
+            set
+            {
+                _CsvRows = value.Where(x => x != null).Skip(1).ToList();
+            }
+
+        }
 
         // 2.
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
